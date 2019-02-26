add547bfd0ca7fd7ea2622ea208c07223be7499f
Jyoti <jyoti.bartaula@gmail.com>
2019-02-26 22:31:32 +0200

    add file upload functionality in single view function

--- app/app.py
+++ app/app.py
@@ -4,7 +4,6 @@ from forms import FeatureForm, FileForm
 import pickle
 import numpy as np
 import pandas as pd
-import os
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -14,19 +13,14 @@ app.config['SECRET_KEY'] = "flask machine learning model"
 def home():
     feature_form = FeatureForm()
     file_form = FileForm()
-    return render_template("home.html", feature_form=feature_form, file_form = file_form)
+    with open ("./static/final_svm_model.pkl",'rb') as pickle_file:
+          model = pickle.load(pickle_file)
 
-@app.route("/feature", methods = ['GET', 'POST'])
-def feature_input():
-    feature_form = FeatureForm()
-    file_form = FileForm()
-    if feature_form.validate_on_submit():
+    if feature_form.validate_on_submit() and feature_form.submit.data:
         sepal_length= feature_form.sepal_length.data
         sepal_width = feature_form.sepal_width.data
         petal_length = feature_form.petal_length.data
         petal_width = feature_form.petal_width.data
-        with open ("./static/final_svm_model.pkl",'rb') as pickle_file:
-              model = pickle.load(pickle_file)
 
         data = np.array([sepal_length,sepal_width, petal_length, petal_width]).reshape(1,-1)
         prediction= model.predict(data)
@@ -40,20 +34,11 @@ def feature_input():
             predict_result = 'virginica'
 
         return render_template("home.html", feature_form=feature_form, file_form = file_form, result = predict_result)
-    #return render_template("home.html", feature_form=feature_form, file_form = file_form)
-    return redirect(url_for("home"))
 
-
-@app.route("/uploadfile", methods = ['GET', 'POST'])
-def file_upload():
-    feature_form = FeatureForm()
-    file_form = FileForm()
-    if file_form.validate_on_submit():
+    elif file_form.validate_on_submit() and file_form.upload.data:
         file = file_form.test_file.data #FileStorage object
         #data = file.read()
         data= pd.read_csv(file,sep=' ', index_col=False)
-        with open ("./static/final_svm_model.pkl",'rb') as model_file:
-              model = pickle.load(model_file)
         prediction = model.predict(data)
         prediction = prediction.astype(str)
         prediction[prediction == '0'] = 'setosa'
@@ -61,8 +46,8 @@ def file_upload():
         prediction[prediction == '2'] = 'virginica'
         np.savetxt("./static/prediction.txt", prediction, fmt="%s")
         return redirect(url_for('predict_txt', filename="prediction.txt"))
+    return render_template("home.html", feature_form=feature_form, file_form = file_form)
 
-    return redirect(url_for("home"))
 
 @app.route('/getPrediction/<filename>') #
 def predict_txt(filename):
--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,3 @@
-#- sepal length in cm\n        - sepal width in cm\n        - petal length in cm\n        - petal width in cm\n
-#array(['setosa', 'versicolor', 'virginica']
-#And the convenience validate_on_submit will check if it is a POST request and if it is valid.
 from flask_wtf import FlaskForm
 from wtforms import DecimalField,SubmitField
 from flask_wtf.file import FileField, FileRequired,FileAllowed
@@ -16,4 +13,4 @@ class FeatureForm(FlaskForm):
 
 class FileForm(FlaskForm):
     test_file = FileField(validators=[FileRequired(), FileAllowed(['txt'])])
-    submit = SubmitField("Upload file")
+    upload = SubmitField("Upload")
